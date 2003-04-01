26b98e03d14c8ca83a4b56c0c89acb064afd2068
Larry Huston <larry.huston@intel.com>
2003-04-01 15:26:54 +0000

    New header file for some common path names.

---
+++ src/lib/libdctl/dctl_common.h
@@ -0,0 +1,25 @@
+#ifndef	_DCTL_COMMON_H_
+#define	_DCTL_COMMON_H_	1
+
+
+/*
+ * This header file file contains some of the common path
+ * names that are used to access resources dctl tree.
+ */
+
+#define	ROOT_PATH				""
+#define	HOST_PATH				"host"
+
+#define	HOST_BACKGROUND			"bg"
+#define	HOST_BACKGROUND_PATH	"host.bg"
+
+#define	NETWORK_NODE			"network"
+#define	HOST_NETWORK_PATH		"host.network"
+
+
+
+#define	DEVICE_PATH				"devices"
+
+#endif	/* !defined(_DCTL_COMMON_H_) */
+
+

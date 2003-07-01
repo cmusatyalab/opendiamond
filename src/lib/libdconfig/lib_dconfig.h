1de8b602750a5e4493f9200a7a02bd284d899836
Rajiv Wickremesinghe <rajiv@cs.duke.edu>
2003-07-01 21:47:29 +0000

    minor changes

--- src/lib/libdconfig/lib_dconfig.h
+++ src/lib/libdconfig/lib_dconfig.h
@@ -6,8 +6,12 @@
  * group ids.
  */
 
+/* returns list of gids for name */
 int nlkup_lookup_collection(char *name, int *num_gids, groupid_t *gids);
+
 int nlkup_add_entry(char *name, int num_gids, groupid_t *gids);
+
+/* iterates through all names of collections (const name). */
 int nlkup_first_entry(char **name, void **cookie);
 int nlkup_next_entry(char **name, void **cookie);
 
--- src/lib/libdconfig/name_test.c
+++ src/lib/libdconfig/name_test.c
@@ -1,6 +1,7 @@
 #include    <sys/types.h>
 #include    <unistd.h>
 #include    <stdio.h>
+#include <stdlib.h>
 #include    <stdint.h>
 #include    <assert.h>
 #include    "lib_od.h"

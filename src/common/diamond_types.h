60d516bc35aba4244f8234292deda7bcf87bfc16
Larry Huston <larry.huston@intel.com>
2003-07-05 18:17:28 +0000

    New header file for shared types.

---
+++ src/common/diamond_types.h
@@ -0,0 +1,18 @@
+#ifndef _DIAMOND_TYPES_H_
+#define	_DIAMOND_TYPES_H_ 
+
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef uint64_t    groupid_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !_DIAMOND_TYPES_H_ */
+

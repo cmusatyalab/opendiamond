c300c0b2ff77aa3e8e293d5d2f4369cadc0f043e
Larry Huston <larry.huston@intel.com>
2004-05-11 02:54:20 +0000

    More code cleanpup for fast paper.

--- src/idiskd/idiskd.c
+++ src/idiskd/idiskd.c
@@ -8,17 +8,19 @@
 #include <stdint.h>
 #include <dirent.h>
 #include "obj_attr.h"
-#include "lib_od.h" 
+#include "lib_od.h"
 #include "lib_odisk.h"
 #include "lib_searchlet.h"
 #include "lib_dctl.h"
 #include "lib_sstub.h"
 #include "filter_exec.h"
 #include "ring.h"
-#include "search_state.h"
+#include "idiskd_ops.h"
 
-/* the default directory that holds our data */
-char *	data_dir = "/opt/dir1/";
+/*
+ * the default directory that holds our data 
+ */
+char           *data_dir = "/opt/dir1/";
 
 
 
@@ -26,118 +28,119 @@ char *	data_dir = "/opt/dir1/";
  * The default behaviors are to create a new daemon at startup time
  * and to fork whenever a new connection is established.
  */
-int	do_daemon = 1;
-int	do_fork = 1;
-int	do_cleanup = 1;
+int             do_daemon = 1;
+int             do_fork = 1;
+int             do_cleanup = 1;
 
 void
 usage()
 {
-	fprintf(stdout, "idiskd [-d] [-n] [-p <pathname>] \n");
-	fprintf(stdout, "\t -d do not run adisk as a daemon \n");
-	fprintf(stdout, "\t -n do not fork for a  new connection \n");
-	fprintf(stdout, "\t -p <pathname>  set alternative data directory \n");
-	fprintf(stdout, "\t -c do not cleanup *.so files from /tmp \n");
-	fprintf(stdout, "\t -h get this help message \n");
+    fprintf(stdout, "idiskd [-d] [-n] [-p <pathname>] \n");
+    fprintf(stdout, "\t -d do not run adisk as a daemon \n");
+    fprintf(stdout, "\t -n do not fork for a  new connection \n");
+    fprintf(stdout, "\t -p <pathname>  set alternative data directory \n");
+    fprintf(stdout, "\t -c do not cleanup *.so files from /tmp \n");
+    fprintf(stdout, "\t -h get this help message \n");
 }
 
 
 int
-main(int argc , char **argv) 
+main(int argc, char **argv)
 {
-	int			err;
-	void *			cookie;
-	sstub_cb_args_t		cb_args;
-	int			c;
+    int             err;
+    void           *cookie;
+    sstub_cb_args_t cb_args;
+    int             c;
 
-	/*
-	 * Parse any of the command line arguments.
-	 */
+    /*
+     * Parse any of the command line arguments.
+     */
 
-	while (1) {
-		c = getopt(argc, argv, "cdhnp:");
-		if (c == -1) {
-			break;
-		}
-		switch (c) {
+    while (1) {
+        c = getopt(argc, argv, "cdhnp:");
+        if (c == -1) {
+            break;
+        }
+        switch (c) {
 
-		case 'c':
-			do_cleanup = 0;
-			break;
+        case 'c':
+            do_cleanup = 0;
+            break;
 
-		case 'd':
-			do_daemon = 0;
-			break;
+        case 'd':
+            do_daemon = 0;
+            break;
 
-		case 'h':
-			usage();
-			exit(0);
-			break;
+        case 'h':
+            usage();
+            exit(0);
+            break;
 
-		case 'n':
-			do_fork = 0;
-			do_daemon = 0;
-			break;
+        case 'n':
+            do_fork = 0;
+            do_daemon = 0;
+            break;
 
-		case 'p':
-			data_dir = optarg;
-			break;
+        case 'p':
+            data_dir = optarg;
+            break;
 
-		default:
-			usage();
-			exit(1);
-			break;
-		}
-			
-	}
+        default:
+            usage();
+            exit(1);
+            break;
+        }
 
-	
-
-	/* make this a daemon by the appropriate call */
-	if (do_daemon) {
-		err = daemon(1, 1);
-		if (err != 0) {
-			perror("daemon call failed !! \n");
-			exit(1);
-		}
-	}
+    }
 
 
-	cb_args.new_conn_cb = search_new_conn;
-	cb_args.close_conn_cb = search_close_conn;
-	cb_args.start_cb = search_start; 
-	cb_args.stop_cb = search_stop;
-	cb_args.set_searchlet_cb = search_set_searchlet; 
-	cb_args.set_list_cb =  search_set_list;
-	cb_args.terminate_cb = search_term;
-	cb_args.get_stats_cb = search_get_stats;
-	cb_args.release_obj_cb = search_release_obj;
-	cb_args.get_char_cb = search_get_char;
-	cb_args.get_stats_cb = search_get_stats;
-	cb_args.log_done_cb = search_log_done;
-	cb_args.setlog_cb = search_setlog;
-	cb_args.rleaf_cb = search_read_leaf;
-	cb_args.wleaf_cb = search_write_leaf;
-	cb_args.lnode_cb = search_list_nodes;
-	cb_args.lleaf_cb = search_list_leafs;
-	cb_args.sgid_cb = search_set_gid;
-	cb_args.clear_gids_cb = search_clear_gids;
-	cb_args.set_blob_cb = search_set_blob;
-	cb_args.set_offload_cb = search_set_offload;
 
-	cookie = sstub_init(&cb_args);
-	if (cookie == NULL) {
-		fprintf(stderr, "Unable to initialize the communications\n");
-		exit(1);
-	}
+    /*
+     * make this a daemon by the appropriate call 
+     */
+    if (do_daemon) {
+        err = daemon(1, 1);
+        if (err != 0) {
+            perror("daemon call failed !! \n");
+            exit(1);
+        }
+    }
 
 
-	/*
-	 * We provide the main thread for the listener.  This
-	 * should never return.
-	 */
-	sstub_listen(cookie, do_fork);
-	
-	exit(0);
+    cb_args.new_conn_cb = search_new_conn;
+    cb_args.close_conn_cb = search_close_conn;
+    cb_args.start_cb = search_start;
+    cb_args.stop_cb = search_stop;
+    cb_args.set_searchlet_cb = search_set_searchlet;
+    cb_args.set_list_cb = search_set_list;
+    cb_args.terminate_cb = search_term;
+    cb_args.get_stats_cb = search_get_stats;
+    cb_args.release_obj_cb = search_release_obj;
+    cb_args.get_char_cb = search_get_char;
+    cb_args.get_stats_cb = search_get_stats;
+    cb_args.log_done_cb = search_log_done;
+    cb_args.setlog_cb = search_setlog;
+    cb_args.rleaf_cb = search_read_leaf;
+    cb_args.wleaf_cb = search_write_leaf;
+    cb_args.lnode_cb = search_list_nodes;
+    cb_args.lleaf_cb = search_list_leafs;
+    cb_args.sgid_cb = search_set_gid;
+    cb_args.clear_gids_cb = search_clear_gids;
+    cb_args.set_blob_cb = search_set_blob;
+    cb_args.set_offload_cb = search_set_offload;
+
+    cookie = sstub_init(&cb_args);
+    if (cookie == NULL) {
+        fprintf(stderr, "Unable to initialize the communications\n");
+        exit(1);
+    }
+
+
+    /*
+     * We provide the main thread for the listener.  This
+     * should never return.
+     */
+    sstub_listen(cookie, do_fork);
+
+    exit(0);
 }
-
--- src/idiskd/idiskd_ops.h
+++ src/idiskd/idiskd_ops.h
@@ -9,27 +9,27 @@
 
 
 typedef struct search_state {
-	void *		        comm_cookie;
-	pthread_t               thread_id;
-	unsigned int            flags;
-	struct odisk_state *    ostate;
-	int                     ver_no;
-	ring_data_t *           control_ops;
-	pthread_mutex_t	    	log_mutex;
-	pthread_cond_t	    	log_cond;
-	pthread_t               log_thread;
-	filter_data_t  *    	fdata;
-	uint		        obj_total;
-	uint		        obj_processed;
-	uint		        obj_dropped;
-	uint			obj_passed;
-	uint			obj_skipped;
-	uint			pend_objs;
-	uint			pend_thresh;
-	uint			bp_feedback;
-	uint			bp_thresh;
-	void *			dctl_cookie;
-	void *			log_cookie;
+    void           *comm_cookie;
+    pthread_t       thread_id;
+    unsigned int    flags;
+    struct odisk_state *ostate;
+    int             ver_no;
+    ring_data_t    *control_ops;
+    pthread_mutex_t log_mutex;
+    pthread_cond_t  log_cond;
+    pthread_t       log_thread;
+    filter_data_t  *fdata;
+    uint            obj_total;
+    uint            obj_processed;
+    uint            obj_dropped;
+    uint            obj_passed;
+    uint            obj_skipped;
+    uint            pend_objs;
+    uint            pend_thresh;
+    uint            bp_feedback;
+    uint            bp_thresh;
+    void           *dctl_cookie;
+    void           *log_cookie;
 } search_state_t;
 
 
@@ -38,29 +38,29 @@ typedef struct search_state {
  * Function prototypes for the search functions.
  */
 
-int search_new_conn(void *cookie, void **app_cookie);
-int search_close_conn(void *app_cookie);
-int search_start(void *app_cookie, int gen_num);
-int search_stop(void *app_cookie, int gen_num);
-int search_set_searchlet(void *app_cookie, int gen_num,
-		char *filter, char *spec);
-int search_set_list(void *app_cookie, int gen_num);
-int search_term(void *app_cookie, int gen_num);
-void search_get_stats(void *app_cookie, int gen_num);
-int search_release_obj(void *app_cookie, obj_data_t *obj);
-int search_get_char(void *app_cookie, int gen_num);
-int search_log_done(void *app_cookie, char *buf, int len);
-int search_setlog(void *app_cookie, uint32_t level, uint32_t src);
-int search_read_leaf(void *app_cookie, char *path, int32_t opid);
-int search_write_leaf(void *app_cookie, char *path, int len, char *data,
-                int32_t opid);
-int search_list_nodes(void *app_cookie, char *path, int32_t opid);
-int search_list_leafs(void *app_cookie, char *path, int32_t opid);
-int search_set_gid(void *app_cookie, int gen, groupid_t gid);
-int search_clear_gids(void *app_cookie, int gen);
-int search_set_blob(void *app_cookie, int gen, char *name,
-				int blob_len, void *blob_data);
-int search_set_offload(void *app_cookie, int gen, uint64_t data);
+int             search_new_conn(void *cookie, void **app_cookie);
+int             search_close_conn(void *app_cookie);
+int             search_start(void *app_cookie, int gen_num);
+int             search_stop(void *app_cookie, int gen_num);
+int             search_set_searchlet(void *app_cookie, int gen_num,
+                                     char *filter, char *spec);
+int             search_set_list(void *app_cookie, int gen_num);
+int             search_term(void *app_cookie, int gen_num);
+void            search_get_stats(void *app_cookie, int gen_num);
+int             search_release_obj(void *app_cookie, obj_data_t * obj);
+int             search_get_char(void *app_cookie, int gen_num);
+int             search_log_done(void *app_cookie, char *buf, int len);
+int             search_setlog(void *app_cookie, uint32_t level, uint32_t src);
+int             search_read_leaf(void *app_cookie, char *path, int32_t opid);
+int             search_write_leaf(void *app_cookie, char *path, int len,
+                                  char *data, int32_t opid);
+int             search_list_nodes(void *app_cookie, char *path, int32_t opid);
+int             search_list_leafs(void *app_cookie, char *path, int32_t opid);
+int             search_set_gid(void *app_cookie, int gen, groupid_t gid);
+int             search_clear_gids(void *app_cookie, int gen);
+int             search_set_blob(void *app_cookie, int gen, char *name,
+                                int blob_len, void *blob_data);
+int             search_set_offload(void *app_cookie, int gen, uint64_t data);
 
 
-#endif	/* ifndef _IDISKD_OPS_H_ */
+#endif                          /* ifndef _IDISKD_OPS_H_ */

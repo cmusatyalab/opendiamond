0c457902b537f24e44e855d14a61899710c5bed1
Larry Huston <larry.huston@intel.com>
2004-07-07 17:26:42 +0000

    Added copyright.

--- src/lib/libdconfig/lib_dconfig.h
+++ src/lib/libdconfig/lib_dconfig.h
@@ -1,9 +1,47 @@
+/*
+ *
+ *
+ *                          Diamond 1.0
+ * 
+ *            Copyright (c) 2002-2004, Intel Corporation
+ *                         All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *    * Neither the name of Intel nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software 
+ *      without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #ifndef _LIB_DCONFIG_H_
 #define _LIB_DCONFIG_H_
 
 
 #ifdef __cplusplus
-extern "C" {
+extern          "C" {
 #endif
 
 
@@ -13,22 +51,22 @@ extern "C" {
  */
 
 /* returns list of gids for name */
-int nlkup_lookup_collection(char *name, int *num_gids, groupid_t *gids);
+int             nlkup_lookup_collection(char *name, int *num_gids,
+                                        groupid_t * gids);
 
-int nlkup_add_entry(char *name, int num_gids, groupid_t *gids);
+int             nlkup_add_entry(char *name, int num_gids,
+                                groupid_t * gids);
 
-/* iterates through all names of collections (const name). */
-int nlkup_first_entry(char **name, void **cookie);
-int nlkup_next_entry(char **name, void **cookie);
+/* iterates through all names of collections (const name).  */
+int             nlkup_first_entry(char **name, void **cookie);
+int             nlkup_next_entry(char **name, void **cookie);
 
-/*
- * Functions that map groups into a set of hosts.
- */
 
-int glkup_gid_hosts(groupid_t gid, int *num_hosts, uint32_t *hostids);
+/* Functions that map groups into a set of hosts.  */
+int             glkup_gid_hosts(groupid_t gid, int *num_hosts,
+                                uint32_t * hostids);
 
 #ifdef __cplusplus
 }
 #endif
-
-#endif  /* !_LIB_DCONFIG_H_ */
+#endif                          /* !_LIB_DCONFIG_H_ */
--- src/lib/libdconfig/name_test.c
+++ src/lib/libdconfig/name_test.c
@@ -1,7 +1,45 @@
+/*
+ *
+ *
+ *                          Diamond 1.0
+ * 
+ *            Copyright (c) 2002-2004, Intel Corporation
+ *                         All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *    * Neither the name of Intel nor the names of its contributors may
+ *      be used to endorse or promote products derived from this software 
+ *      without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
 #include    <sys/types.h>
 #include    <unistd.h>
 #include    <stdio.h>
-#include <stdlib.h>
+#include    <stdlib.h>
 #include    <stdint.h>
 #include    <assert.h>
 #include    "lib_od.h"
@@ -13,15 +51,15 @@
 int
 enum_names()
 {
-    void *cookie;
-    char *name;
-    int         err;
+    void           *cookie;
+    char           *name;
+    int             err;
 
     err = nlkup_first_entry(&name, &cookie);
     assert(err == 0);
     printf("name:  %s \n", name);
 
-    while (1) { 
+    while (1) {
         err = nlkup_next_entry(&name, &cookie);
         if (err) {
             break;
@@ -29,17 +67,17 @@ enum_names()
         printf("name:  %s \n", name);
     }
 
-    return(0);
+    return (0);
 }
 
 void
 dump_gid_list()
 {
-    void *cookie;
-    char *name;
-    int         err;
-    int         num, i;
-    groupid_t   gid_list[MAX_GIDS];
+    void           *cookie;
+    char           *name;
+    int             err;
+    int             num, i;
+    groupid_t       gid_list[MAX_GIDS];
     num = MAX_GIDS;
 
 
@@ -48,15 +86,15 @@ dump_gid_list()
 
     err = nlkup_lookup_collection(name, &num, gid_list);
     assert(err == 0);
-    
+
     fprintf(stdout, "%s: ", name);
-    for (i=0; i < num; i++) {
-       fprintf(stdout, "%016llx ", gid_list[i]);
+    for (i = 0; i < num; i++) {
+        fprintf(stdout, "%016llx ", gid_list[i]);
     }
     fprintf(stdout, "\n");
 
 
-    while (1) { 
+    while (1) {
         err = nlkup_next_entry(&name, &cookie);
         if (err) {
             break;
@@ -65,7 +103,7 @@ dump_gid_list()
         err = nlkup_lookup_collection(name, &num, gid_list);
         assert(err == 0);
         fprintf(stdout, "%s: ", name);
-        for (i=0; i < num; i++) {
+        for (i = 0; i < num; i++) {
             fprintf(stdout, "%016llx ", gid_list[i]);
         }
         fprintf(stdout, "\n");
@@ -77,8 +115,8 @@ dump_gid_list()
 int
 main(int argc, char **argv)
 {
-    groupid_t   gid_list[MAX_GIDS];
-    int         num, err, i;
+    groupid_t       gid_list[MAX_GIDS];
+    int             num, err, i;
 
     err = enum_names();
     dump_gid_list();
@@ -89,7 +127,7 @@ main(int argc, char **argv)
         fprintf(stdout, "failed lookup on <foo> \n");
     } else {
         fprintf(stdout, "foo ");
-        for (i=0; i < num; i++) {
+        for (i = 0; i < num; i++) {
             printf("%016llx ", gid_list[i]);
         }
         fprintf(stdout, "\n");
@@ -99,7 +137,7 @@ main(int argc, char **argv)
     err = lookup_add_name("foo1", 1, gid_list);
     assert(err == 0);
 #endif
-    
+
 
     exit(0);
 }

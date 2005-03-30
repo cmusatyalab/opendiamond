f24e8c0d705ff3d0b01a390641e2eabfdabbc50b
Larry Huston <larry.huston@intel.com>
2005-03-30 06:44:34 +0000

    Initail checkin

---
+++ scripts/gen_diamond.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+RELEASE_NAME=diamond-l.0.0
+#
+# Make sure we have cleaned the source tree
+#
+pushd ../src
+export DIAMOND_ROOT=`pwd`
+make clean
+popd
+
+#
+# Remove the old version in /tmp if it exists
+#
+rm -rf /tmp/"$RELEASE_NAME"
+
+#
+# copy over the whole tree
+#
+cp -r ../src /tmp/"$RELEASE_NAME"/
+
+# remove the CVS directories
+find /tmp/"$RELEASE_NAME" -name CVS -exec rm -rf {} \;
+
+# generate the tar file
+
+pushd /tmp
+tar -cf /tmp/"$RELEASE_NAME".tar "$RELEASE_NAME"
+
+popd
+cp /tmp/"$RELEASE_NAME".tar .
+
+gzip "$RELEASE_NAME".tar 

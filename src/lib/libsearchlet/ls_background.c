100132d37904427df661116c3ee8dd4a2d9ec426
Larry Huston <larry@thehustons.net>
2006-01-27 03:58:09 +0000

    Fix bug when shutting down the host processing

--- src/lib/libsearchlet/ls_background.c
+++ src/lib/libsearchlet/ls_background.c
@@ -12,6 +12,15 @@
  */
 
 /*
+ *  Copyright (c) 2006 Larry Huston <larry@thehustons.net>
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+/*
  * These file handles a lot of the device specific code.  For the current
  * version we have state for each of the devices.
  */
@@ -765,7 +774,7 @@ bg_stop_search(search_context_t * sc, int id)
 		 */
 		return (0);
 	}
-	cmd->cmd = BG_START;
+	cmd->cmd = BG_STOP;
 	ring_enq(sc->bg_ops, (void *) cmd);
 	return (0);
 }

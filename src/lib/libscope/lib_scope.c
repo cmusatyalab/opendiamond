bb08169b2e08747d9bbb358ded2aa925f2172e4c
Adam Wolbach <awolbach@andrew.cmu.edu>
2007-01-15 17:30:54 +0000

    Creation of lib/libscope to encapsulate work on the new metadata scoping architecture.  Added preliminary API header and source files, as well as Makefile.in.

---
+++ src/lib/libscope/lib_scope.c
@@ -0,0 +1,28 @@
+/*
+ *      Diamond
+ *      A system for interactive brute-force search
+ *
+ *      Copyright (c) 2002-2007, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
+/*
+ *  Copyright (c) 2007 Adam Wolbach
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
+/*
+ * This library provides the main functions of the dynamic
+ * metadata scoping API.
+ */
---
+++ src/lib/libscope/lib_scope.h
@@ -0,0 +1,42 @@
+/*
+ * 	Diamond (Release 1.0)
+ *      A system for interactive brute-force search
+ *
+ *      Copyright (c) 2002-2007, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
+/*
+ *  Copyright (c) 2007 Adam Wolbach
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+#ifndef _LIB_SEARCHLET_H_
+#define	_LIB_SEARCHLET_H_
+
+/*!  
+ * \defgroup scope  Metadata Scoping API
+ * The metadata scoping API is a programming interface between the core
+ * Diamond system and the Diamond applications running on the host. All 
+ * metadata scoping operations with Diamond are performed through this 
+ * interface.  The programming interface is defined in lib_scope.h.
+ *
+ * */
+
+
+/*!
+ * \file lib_scope.h
+ * \ingroup scope
+ * This defines the API that applications use to set the scope of
+ * future searches in the Diamond system.
+ */

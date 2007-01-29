9aa48a2859f0b5112293d5aa8c12547e003b974b
Adam Goode <agoode@andrew.cmu.edu>
2007-01-29 21:52:09 +0000

    fix C

--- src/lib/libscope/lib_scope.c
+++ src/lib/libscope/lib_scope.c
@@ -2,7 +2,7 @@
  *      OpenDiamond 2.0
  *      A system for interactive brute-force search
  *
- *  Copyright (c) 2007 Carnegie Mellon University
+ *  Copyright (c) 2007 Adam Wolbach and Carnegie Mellon University
  *
  *  This software is distributed under the terms of the Eclipse Public
  *  License, Version 1.0 which can be found in the file named LICENSE.

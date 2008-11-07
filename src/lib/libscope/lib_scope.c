5dcf136d7322f1c8f78d7f83eb0f35bb7279f3a4
Jan Harkes <jaharkes@cs.cmu.edu>
2008-11-07 15:22:58 -0500

    Clean up parsing of the NEWSCOPE file.
    Don't leak file handles when rotating the name and gid map files.

--- src/lib/libscope/lib_scope.c
+++ src/lib/libscope/lib_scope.c
@@ -2,7 +2,7 @@
  *  The OpenDiamond Platform for Interactive Search
  *  Version 4
  *
- *  Copyright (c) 2007 Carnegie Mellon University
+ *  Copyright (c) 2007-2008 Carnegie Mellon University
  *  All rights reserved.
  *
  *  This software is distributed under the terms of the Eclipse Public
@@ -19,205 +19,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #include <sys/param.h>
+#include <unistd.h>
 #include "lib_scope.h"
 
-#define INT_CHARSIZE 16
 
-int
-ls_define_scope(void) {
-  FILE *fp, *np = NULL, *gp = NULL, *rot;
-  char ns[INT_CHARSIZE], gs[INT_CHARSIZE], path[MAXPATHLEN], *home;
-  unsigned int namemap_size, gidmap_size;
-  int i;
+#define MAX_ROTATE_FILENO 9
 
+/* rotate a set of files */
+static void rotate(const char *path)
+{
+    char buf[2][MAXPATHLEN], *cur, *prev = NULL;
+    unsigned int i;
 
-  if((home = getenv("HOME")) == NULL) {
-    fprintf(stderr, "libscope: Couldn't get user's home directory!\n");
-    return -1;
-  }
-
-
-  /* If there is no NEWSCOPE file, punt early and don't screw up
-     the rotated map file history. */
-
-  snprintf(path, MAXPATHLEN, "%s/.diamond/NEWSCOPE", home);
-  if((fp = fopen(path, "r")) == NULL) {
-    fprintf(stderr, "libscope: Couldn't open %s!\n", path);
-    return 0;
-  }
-  fclose(fp); fp = NULL;
-
-
-  /* Rotate old name_map files out of the way. */
-
-  i=1, rot = NULL;
-  do {
-    if(rot != NULL) fclose(rot);
-    snprintf(path, MAXPATHLEN, "%s/.diamond/name_map-%d", home, i);
-    if((rot = fopen(path, "r")) != NULL)
-      i++;
-  }
-  while(rot != NULL);
-  
-  for(; i>1; i--) {
-    char oldpath[MAXPATHLEN], newpath[MAXPATHLEN];
-
-    snprintf(oldpath, MAXPATHLEN, "%s/.diamond/name_map-%d", home, i-1);
-    snprintf(newpath, MAXPATHLEN, "%s/.diamond/name_map-%d", home, i);
-
-    if(rename(oldpath, newpath) < 0) {
-      perror("rename");
-      fprintf(stderr, "Failed executing rename from %s to %s\n", 
-	      oldpath, newpath);
+    for (i = MAX_ROTATE_FILENO; i > 0; i--)
+    {
+	cur = buf[i % 2];
+	snprintf(cur, MAXPATHLEN, "%s-%u", path, i);
+	if (prev) rename(cur, prev);
+	prev = cur;
     }
-  }
-  snprintf(path, MAXPATHLEN, "%s/.diamond/name_map", home);
-  if((rot = fopen(path, "r")) != NULL) {
-    char oldpath[MAXPATHLEN], newpath[MAXPATHLEN];
+    rename(path, prev);
+}
 
-    fclose(rot);
-
-    snprintf(oldpath, MAXPATHLEN, "%s/.diamond/name_map", home);
-    snprintf(newpath, MAXPATHLEN, "%s/.diamond/name_map-1", home);
-
-    if(rename(oldpath, newpath) < 0) {
-      perror("rename");
-      fprintf(stderr, "Failed executing rename from %s to %s\n", 
-	      oldpath, newpath);
-    }
-  }
-
-
-  /* Rotate old gid_map files out of the way. */
-
-  i=1, rot = NULL;
-  do {
-    if(rot != NULL) fclose(rot);
-    snprintf(path, MAXPATHLEN, "%s/.diamond/gid_map-%d", home, i);
-    rot = fopen(path, "r");
-    if((rot = fopen(path, "r")) != NULL)
-      i++;
-  }
-  while(rot != NULL);
-  
-  for(; i>1; i--) {
-    char oldpath[MAXPATHLEN], newpath[MAXPATHLEN];
-
-    snprintf(oldpath, MAXPATHLEN, "%s/.diamond/gid_map-%d", home, i-1);
-    snprintf(newpath, MAXPATHLEN, "%s/.diamond/gid_map-%d", home, i);
-
-    if(rename(oldpath, newpath) < 0) {
-      perror("rename");
-      fprintf(stderr, "Failed executing rename from %s to %s\n", 
-	      oldpath, newpath);
-    }
-  }
-  snprintf(path, MAXPATHLEN, "%s/.diamond/gid_map", home);
-  if((rot = fopen(path, "r")) != NULL) {
-    char oldpath[MAXPATHLEN], newpath[MAXPATHLEN];
-
-    snprintf(oldpath, MAXPATHLEN, "%s/.diamond/gid_map", home);
-    snprintf(newpath, MAXPATHLEN, "%s/.diamond/gid_map-1", home);
-
-    if(rename(oldpath, newpath) < 0) {
-      perror("rename");
-      fprintf(stderr, "Failed executing rename from %s to %s\n", 
-	      oldpath, newpath);
-    }
-  }
-
-
-  /* Now that we have created space, parse the NEWSCOPE file and write
-   * out new name_map and gid_map files for the Diamond application to read. */
-
-  snprintf(path, MAXPATHLEN, "%s/.diamond/NEWSCOPE", home);
-  if((fp = fopen(path, "r")) == NULL) {
-    fprintf(stderr, "Couldn't open scope file %s!\n", path);
-    goto exit_failure;
-  }
-
-  snprintf(path, MAXPATHLEN, "%s/.diamond/name_map", home);
-  if((np = fopen(path, "w")) == NULL) {
-    fprintf(stderr, "Couldn't open %s!\n", path);
-    goto exit_failure;
-  }
-
-  snprintf(path, MAXPATHLEN, "%s/.diamond/gid_map", home);
-  if((gp = fopen(path, "w")) == NULL) {
-    fprintf(stderr, "Couldn't open %s!\n", path);
-    goto exit_failure;
-  }
-
-
-  /* Read name_map size and data. */
-
-  if(fgets(ns, INT_CHARSIZE, fp) == NULL) {
-    fprintf(stderr, "couldn't read name_map size\n");
-    goto exit_failure;
-  }
-  else {
-    int len = strlen(ns);
-    ns[len-1]='\0';  //remove '\n'
-  }
-
-  namemap_size = atoi(ns);
-
-  for(i=0; i<namemap_size; i++) {
+static int copy_mapdata(const char *name, FILE *fp)
+{
     char line[NCARGS];
-    
-    if(fgets(line, NCARGS, fp) == NULL) {
-      fprintf(stderr, "couldn't read name_map data\n");
-      goto exit_failure;
-    }
-    
-    if((fwrite(line, strlen(line), 1, np)) != 1) {
-      fprintf(stderr, "couldn't write name_map data\n");
-      goto exit_failure;
-    }
-  }
+    unsigned int i, nlines;
+    size_t len;
+    int out, err;
 
-
-  /* Read gid_map size and data. */
-
-  if(fgets(gs, INT_CHARSIZE, fp) == NULL) {
-    fprintf(stderr, "couldn't read gid_map size\n");
-    goto exit_failure;
-  }
-  else {
-    int len = strlen(gs);
-    gs[len-1]='\0';  //remove '\n'
-  }
-  
-  gidmap_size = atoi(gs);
-
-  for(i=0; i<gidmap_size; i++) {
-    char line[NCARGS];
-    
-    if(fgets(line, NCARGS, fp) == NULL) {
-      fprintf(stderr, "couldn't read name_map data\n");
-      goto exit_failure;
+    /* Read nlines. */
+    err = fscanf(fp, "%u\n", &nlines);
+    if (err != 1) {
+	fprintf(stderr, "couldn't read %s size\n", name);
+	return -1;
     }
 
-    if((fwrite(line, strlen(line), 1, gp)) != 1) {
-      fprintf(stderr, "couldn't write name_map data\n");
-      goto exit_failure;
+    /* Copy data. */
+    out = open(name, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+    if (out == -1) {
+	fprintf(stderr, "couldn't open %s for writing\n", name);
+	return -1;
     }
-  }
 
+    err = 0;
+    for (i = 0; i < nlines; i++) {
+	if (fgets(line, NCARGS, fp) == NULL) {
+	    fprintf(stderr, "couldn't read map data for %s\n", name);
+	    err = -1;
+	    break;
+	}
 
-  if(fp) fclose(fp);
-  if(np) fclose(np);
-  if(gp) fclose(gp);
+	len = strlen(line);
+	if (write(out, line, len) != (ssize_t)len) {
+	    fprintf(stderr, "couldn't write map data for %s\n", name);
+	    err = -1;
+	    break;
+	}
+    }
 
-  return 0;
+    close(out);
+    return err;
+}
 
+/* Parse the NEWSCOPE file and write out name_map and gid_map files for the
+ * Diamond application to read. */
+int ls_define_scope(void)
+{
+    FILE *fp;
+    char path[MAXPATHLEN], *home;
+    int err;
 
- exit_failure:
+    home = getenv("HOME");
+    if (!home) {
+	fprintf(stderr, "libscope: Couldn't get user's home directory!\n");
+	return -1;
+    }
 
-  if(fp) fclose(fp);
-  if(np) fclose(np);
-  if(gp) fclose(gp);
+    snprintf(path, MAXPATHLEN, "%s/.diamond/NEWSCOPE", home);
+    fp = fopen(path, "r");
+    if (fp == NULL) {
+	fprintf(stderr, "Couldn't open scope file %s!\n", path);
+	return 0;
+    }
 
-  return -1;
+    /* Rotate old name_map files out of the way and write out the new file. */
+    snprintf(path, MAXPATHLEN, "%s/.diamond/name_map", home);
+    rotate(path);
+    err = copy_mapdata(path, fp);
+    if (err) goto exit;
+
+    /* Rotate old gid_map files out of the way and write out the new file. */
+    snprintf(path, MAXPATHLEN, "%s/.diamond/gid_map", home);
+    rotate(path);
+    err = copy_mapdata(path, fp);
+    if (err) goto exit;
+
+exit:
+    fclose(fp);
+    return err;
 }

1e306c5544577f84a50e3b615ad2dbeefd22acac
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2011-05-16 15:57:15 -0400

    Move daemonize code from dataretriever into opendiamond.helpers
    Also, query for the maximum fd to close rather than hardcoding 1024.

--- lib/python/opendiamond/helpers.py
+++ lib/python/opendiamond/helpers.py
@@ -1,8 +1,8 @@
 #
 #  The OpenDiamond Platform for Interactive Search
-#  Version 4
+#  Version 6
 #
-#  Copyright (c) 2009 Carnegie Mellon University
+#  Copyright (c) 2009-2011 Carnegie Mellon University
 #  All rights reserved.
 #
 #  This software is distributed under the terms of the Eclipse Public
@@ -11,3 +11,24 @@
 #  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
 #
 
+import os
+import resource
+
+def daemonize():
+    # Double-fork
+    if os.fork():
+        os._exit(0)
+    os.setsid()
+    if os.fork():
+        os._exit(0)
+    # Close open fds
+    maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+    for fd in xrange(maxfd):
+        try:
+            os.close(fd)
+        except OSError:
+            pass
+    # Open new fds
+    os.open("/dev/null", os.O_RDWR)
+    os.dup2(0, 1)
+    os.dup2(0, 2)

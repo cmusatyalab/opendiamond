7e20d4b412a2f11119b2e5351b8bf536c1604346
Trevor Narayan <tnarayan@cs.cmu.edu>
2011-06-27 11:05:56 -0400

    Added a cache for GigaPan metadata

--- dataretriever/gigapan_store.py
+++ dataretriever/gigapan_store.py
@@ -21,6 +21,8 @@ OBJECT_URI = 'obj'
 # include a reference to xslt stylesheet (only useful for debugging)
 STYLE = False
 
+from datetime import datetime, timedelta
+from threading import Lock
 from dataretriever.util import guess_mime_type
 from wsgiref.util import shift_path_info
 from urllib import quote
@@ -32,7 +34,17 @@ import re
 
 __all__ = ['scope_app', 'object_app']
 
-gigapan_info = 0
+class GigaPanInfoCache:
+    def __init__(self):
+        self._cache = {}
+        self._lock = Lock()
+    def __getitem__(self, id):
+        with self._lock:
+            if id not in self._cache:
+                self._cache[id] = stat_gigapan(id)
+            return self._cache[id]
+
+gigapan_info_cache = GigaPanInfoCache()
 
 def scope_app(environ, start_response):
     root = shift_path_info(environ)
@@ -49,11 +61,10 @@ def ExpandUrls (id):
     yield '<?xml version="1.0" encoding="UTF-8" ?>\n'
     yield '<objectlist>\n'
 
-    global gigapan_info
-    gigapan_info = stat_gigapan(id)
-    height = gigapan_info.get('height')
-    width = gigapan_info.get('width')
-    levels = gigapan_info.get('levels')
+    info = gigapan_info_cache[id]
+    height = info.get('height')
+    width = info.get('width')
+    levels = info.get('levels')
     yield 'Height: %d, Width: %d, Levels: %d' % (height, width, levels)
 
     # still calculating stupidly
@@ -71,7 +82,8 @@ def ExpandUrls (id):
     try:
         while 1:
             coord = iter.next()
-            yield '<object src="obj/%s/%s/%s/%s"/>\n' % (id, coord[0], coord[1], coord[2])
+            yield '<object src="obj/%s/%s/%s/%s"/>\n' % (id, coord[0], 
+                                                         coord[1], coord[2])
     except (StopIteration):
         pass
     
@@ -81,27 +93,32 @@ def ExpandUrls (id):
 
 def object_app(environ, start_response):
     components = environ['PATH_INFO'][1:].split('/')
-    id = components[0]
+    id = int(components[0])
     lvl = int(components[1])
     col = int(components[2])
     row = int(components[3])
-    url_components = ["http://share.gigapan.org/gigapans0/", str(id), '/tiles/',path_to_tile(lvl, col, row)]
+    url_components = ["http://share.gigapan.org/gigapans0/", str(id),
+                      '/tiles/', path_to_tile(lvl, col, row)]
     url = ''.join(url_components)
 
     obj = urlopen(url)
-    global gigapan_info
+    time = datetime.utcnow() + timedelta(days=365)
+    timestr = time.strftime('%a, %d %b %Y %H:%M:%S GMT')
 
+    info = gigapan_info_cache[id]
     headers = [ # copy some headers for caching purposes
         ('Content-Length',		obj.headers['Content-Length']),
         ('Content-Type',		obj.headers['Content-Type']),
         ('Last-Modified',		obj.headers['Last-Modified']),
-        ('x-attr-gigapan_id',           id),
-        ('x-attr-gigapan_height',       gigapan_info.get('height')),
-        ('x-attr-gigapan_width',        gigapan_info.get('width')),
-        ('x-attr-gigapan_levels',       gigapan_info.get('levels')),
-        ('x-attr-tile_level',        lvl),
-        ('x-attr-tile_col',          col),
-        ('x-attr-tile_row',          row),
+        ('Expires',                     timestr),
+
+        ('x-attr-gigapan_id',           str(id)),
+        ('x-attr-gigapan_height',       str(info.get('height'))),
+        ('x-attr-gigapan_width',        str(info.get('width'))),
+        ('x-attr-gigapan_levels',       str(info.get('levels'))),
+        ('x-attr-tile_level',        str(lvl)),
+        ('x-attr-tile_col',          str(col)),
+        ('x-attr-tile_row',          str(row)),
         ]
     print(headers)
     start_response("200 OK", headers)

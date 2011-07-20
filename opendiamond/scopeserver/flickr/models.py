e165a9a114a8eb03cca3fba1f9c23f65c3a341fe
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2011-07-20 15:50:56 -0400

    opendiamond.scopeserver.flickr: Typographic fix

--- opendiamond/scopeserver/flickr/models.py
+++ opendiamond/scopeserver/flickr/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Access(models.Model):
     class Meta:
-	permissions = ( ("search", "Can search photos through Flickr API"), )
+        permissions = ( ("search", "Can search photos through Flickr API"), )
 

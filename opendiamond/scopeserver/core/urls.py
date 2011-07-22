d8fe0b4363201143056f6183c9144a1ccd8bf6bd
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2011-07-22 15:18:12 -0400

    scopeserver: Add core application

---
+++ opendiamond/scopeserver/core/urls.py
@@ -0,0 +1,18 @@
+#
+#  The OpenDiamond Platform for Interactive Search
+#  Version 6
+#
+#  Copyright (c) 2011 Carnegie Mellon University
+#  All rights reserved.
+#
+#  This software is distributed under the terms of the Eclipse Public
+#  License, Version 1.0 which can be found in the file named LICENSE.
+#  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+#  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+#
+
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('opendiamond.scopeserver.core.views',
+    (r'^$', 'index'),
+)
---
+++ opendiamond/scopeserver/core/views.py
@@ -0,0 +1,19 @@
+#
+#  The OpenDiamond Platform for Interactive Search
+#  Version 6
+#
+#  Copyright (c) 2010-2011 Carnegie Mellon University
+#  All rights reserved.
+#
+#  This software is distributed under the terms of the Eclipse Public
+#  License, Version 1.0 which can be found in the file named LICENSE.
+#  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+#  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+#
+
+from django.contrib.auth.decorators import login_required
+from opendiamond.scopeserver import render_response
+
+@login_required
+def index(request):
+    return render_response(request, "scopeserver/home.html")

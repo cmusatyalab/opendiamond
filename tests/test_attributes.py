6b71d7332fc99fee0a36770c295143d16ea227a6
Jan Harkes <jaharkes@cs.cmu.edu>
2017-02-22 14:42:20 -0500

    Add tests for opendiamond.attributes

---
+++ tests/test_attributes.py
@@ -0,0 +1,152 @@
+#
+#  The OpenDiamond Platform for Interactive Search
+#
+#  Copyright (c) 2017 Carnegie Mellon University
+#  All rights reserved.
+#
+#  This software is distributed under the terms of the Eclipse Public
+#  License, Version 1.0 which can be found in the file named LICENSE.
+#  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+#  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+#
+
+import math
+import struct
+
+import PIL.Image
+import pytest
+
+import opendiamond.attributes
+
+
+def test_string_attr():
+    codec = opendiamond.attributes.StringAttributeCodec()
+    assert codec.encode("test") == b'test\0'
+    assert codec.decode(b'test\0') == "test"
+
+    # conversion to string
+    assert codec.encode(u"test") == b'test\0'
+    assert codec.encode(1) == b'1\0'
+
+    # bad inputs
+    with pytest.raises(ValueError):
+        assert codec.decode(b'test')
+        assert codec.decode(b'\0\0')
+
+
+def test_integer_attr():
+    codec = opendiamond.attributes.IntegerAttributeCodec()
+    assert codec.encode(1) == b'\1\0\0\0'
+    assert codec.decode(b'\1\0\0\0') == 1
+    assert codec.encode(-1) == b'\xff\xff\xff\xff'
+    assert codec.decode(b'\xff\xff\xff\xff') == -1
+
+    # implicit conversion to int
+    assert codec.encode(1.0) == b'\1\0\0\0'
+
+    # bad inputs
+    with pytest.raises(struct.error):
+        assert codec.encode("test")
+        assert codec.decode(b'\1')
+
+
+def test_float_attr():
+    codec = opendiamond.attributes.DoubleAttributeCodec()
+
+    assert codec.encode(1.0) == b'\0\0\0\0\0\0\xf0?'
+    assert codec.decode(b'\0\0\0\0\0\0\xf0?') == 1.0
+    assert codec.encode(float('inf')) == b'\0\0\0\0\0\0\xf0\x7f'
+    assert math.isinf(codec.decode(b'\0\0\0\0\0\0\xf0\x7f'))
+    assert codec.encode(float('nan')) == b'\0\0\0\0\0\0\xf8\x7f'
+    assert math.isnan(codec.decode(b'\0\0\0\0\0\0\xf8\x7f'))
+
+    # implicit conversion to float
+    assert codec.encode(1) == b'\0\0\0\0\0\0\xf0?'
+
+    # bad inputs
+    with pytest.raises(struct.error):
+        assert codec.encode("test")
+        assert codec.decode(b'\0')
+
+
+def test_rgbimage_attr():
+    codec = opendiamond.attributes.RGBImageAttributeCodec()
+
+    # RGB image
+    image = PIL.Image.new('RGB', (1, 1))
+    rawimage = b'\0\0\0\0\x14\0\0\0\1\0\0\0\1\0\0\0\0\0\0\0'
+    assert codec.encode(image) == rawimage
+    assert codec.decode(rawimage) == image
+
+    with pytest.raises(SystemError):
+        # 0x0 pixel RGB image
+        image = PIL.Image.new('RGB', (0, 0))
+        assert codec.encode(image)
+
+    with pytest.raises(ValueError):
+        # no packer from RGBA to RGBX
+        image = PIL.Image.new('RGBA', (1, 1))
+        assert codec.encode(image)
+
+        # no packer from greyscale
+        image = PIL.Image.new('L', (1, 1))
+        assert codec.encode(image)
+
+        # no packer from bitmap
+        image = PIL.Image.new('1', (1, 1))
+        assert codec.encode(image)
+
+        # not enough image data
+        assert codec.decode(rawimage[:-2])
+
+        # invalid height in image header
+        badimage = rawimage[:8] + b'\2' + rawimage[9:]
+        assert codec.decode(badimage)
+
+    with pytest.raises(struct.error):
+        # truncated image data
+        assert codec.decode(b'')
+        assert codec.decode(rawimage[:2])
+
+
+def test_patches_attr():
+    codec = opendiamond.attributes.PatchesAttributeCodec()
+
+    nopatches = b'\0\0\0\0\0\0\0\0\0\0\xf0?'
+    assert codec.encode((1.0, [])) == nopatches
+    assert codec.decode(nopatches) == (1.0, ())
+
+    onepatch = b'\1\0\0\0\0\0\0\0\0\0\xf0?\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0'
+    assert codec.encode((1.0, [((0, 0), (0, 0))])) == onepatch
+    assert codec.decode(onepatch) == (1.0, (((0, 0), (0, 0)),))
+
+    # implicit conversions
+    assert codec.encode((1, [])) == nopatches
+    assert codec.encode((1.0, ())) == nopatches
+
+    with pytest.raises(struct.error):
+        assert codec.decode(onepatch[:-1])
+
+        badpatch = b'\2' + onepatch[1:]
+        assert codec.decode(badpatch)
+
+
+def test_heatmap_attr():
+    codec = opendiamond.attributes.HeatMapAttributeCodec()
+
+    heatmap = PIL.Image.new('L', (1, 1))
+    raw_heatmap = (
+        b"\x89PNG\r\n\x1a\n\0\0\0\rIHDR\0\0\0\1\0\0\0\1\x08\0\0\0\0:~\x9b" +
+        b"U\0\0\0\nIDATx\xdac`\0\0\0\2\0\1\xe5'\xde\xfc\0\0\0\0IEND\xaeB`\x82")
+    assert codec.encode(heatmap) == raw_heatmap
+
+    # cannot compare directly because decoded image is 'PIL.PngImagePlugin'
+    # and heatmap is PIL.Image.Image
+    assert codec.decode(raw_heatmap).format == 'PNG'
+    assert codec.decode(raw_heatmap).mode == heatmap.mode
+    assert codec.decode(raw_heatmap).size == heatmap.size
+    assert codec.decode(raw_heatmap).load()[0, 0] == heatmap.load()[0, 0]
+
+    # heatmaps are always converted to greyscale
+    heatmap = PIL.Image.new('RGB', (1, 1))
+    assert codec.encode(heatmap) == raw_heatmap

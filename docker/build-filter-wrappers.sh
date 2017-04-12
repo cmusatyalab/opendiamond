976f83e1722c8ac1f5822ec4994b2be41193d8e8
Jan Harkes <jaharkes@cs.cmu.edu>
2017-04-12 13:59:45 -0400

    Avoiding libdc1394 errors is not a generic opendiamond issue

--- docker/build-filter-wrappers.sh
+++ docker/build-filter-wrappers.sh
@@ -14,7 +14,7 @@ for filter in /usr/local/share/diamond/filters/* ; do
     cat > $filter << EOF
 #!/bin/sh
 docker pull $IMAGEID >/dev/null 2>&1
-exec docker run --rm -i --log-driver=none -v/dev/null:/dev/raw1394 --entrypoint=$filter $IMAGEID "\$@"
+exec docker run --rm -i --log-driver=none --entrypoint=$filter $IMAGEID "\$@"
 EOF
 done
 

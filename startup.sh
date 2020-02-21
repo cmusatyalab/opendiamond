500dab10f3fd8f363b872c0f2ea29f6f6f5a9016
root <root@briolette.diamond.cs.cmu.edu>
2020-02-21 14:08:18 -0500

    change startup

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@ cd $USER/opendiamond
 python setup.py install 
 systemctl restart diamondd
 systemctl restart dataretriever
-systemctl is-active --quiet diamondd && echo diamondd is running
-systemctl is-active --quiet dataretriever && echo dataretriever is running
+#systemctl is-active --quiet diamondd #&& echo "diamondd is running" || echo "diamondd is NOT running"
+systemctl is-active diamondd #&& echo "diamondd is running" || echo "diamondd is NOT running"
+systemctl is-active dataretriever #&& echo "dataretriever is running" || echo "dataretriever is NOT running"
 
